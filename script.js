458e0a077b80c554b4f44a25bb98893c81731573
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-01-29 17:10:37 -0300

    finalizado primeira parte do portifolio

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
 const skills = [
-    {name: "html", porcentagem: "40%"},
-    {name: "next", porcentagem: "10%"},
-    {name: "vue", porcentagem: "56%"},
+    {name: "html", porcentagem: "84%"},
+    // {name: "next", porcentagem: "10%"},
+    {name: "vue", porcentagem: "50%"},
     {name: "react", porcentagem: "90%"},
-    {name: "nest", porcentagem: "56%"},
+    {name: "nest", porcentagem: "68%"},
     {name: "sass", porcentagem: "50%"},
-    {name: "node", porcentagem: "50%"},
+    {name: "node", porcentagem: "65%"},
     // {name: "javascript", porcentagem: "50%"},
     {name: "typescript", porcentagem: "50%"},
-    {name: "css", porcentagem: "50%"},
+    {name: "css", porcentagem: "85%"},
 ]
 
 skills.map((item) => {
-    $(".habilidades").append(`
+    $(".skills").append(`
     <div>
-        <div class="name"><b>${item.name}</b></div>
-        <div class="barra-fundo">
-            <div style="width: ${item.porcentagem}"class="barra-dentro"></div>
+        <div style="font-size: 12px; text-transform: uppercase;"><b>${item.name}</b></div>
+        <div class="bar-out">
+            <div style="width: ${item.porcentagem} "class="inside-bar"></div>
         </div>
     </div>
     `)
 })
 
 $(".logo-discord").click(() => {
-    window.location = "https://discord.gg/CjbzggEerU"
+    window.open("https://discord.gg/CjbzggEerU");
 })
+
 $(".logo-linkedin").click(() => {
-    window.location = "https://www.linkedin.com/in/vinicius-benfica/"
+    window.open("https://www.linkedin.com/in/vinicius-benfica/");
 })
+
 $(".logo-instagram").click(() => {
-    window.location = "https://www.instagram.com/vb_benfica/"
+    window.open("https://www.instagram.com/vb_benfica/");
 })
+
 $(".logo-github").click(() => {
-    window.location = "https://github.com/ViniciusBenfica/"
+    window.open("https://github.com/ViniciusBenfica/");
 })
 
 $(window).scroll(() => {
     let scroll = $(window).scrollTop();
     if(scroll >= 500){
-        $(".barra-dentro").show()
-        $(".barra-dentro").addClass("animation-inside-bar")
+        $(".about").fadeIn()
+        $(".inside-bar").show()
+        $(".inside-bar").addClass("animation-inside-bar")
     }
 });
 
-$(".return-last-place").click(() => {
-    window.scrollTo({top: 850, behavior: 'smooth'})
+$(".see-more").click(() => {
+    var scroll = $("section").height()
+    window.scrollTo({top: scroll, behavior: 'smooth'})
 })
